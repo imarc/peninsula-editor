0c1f943140a0fdb0b0028cb769c7e59b5173612e
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2022-01-12 09:13:38 -0800

    Modularize CKEditor plugins and config

---
+++ src/ckplugins.js
@@ -0,0 +1,30 @@
+import InlineEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor'
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat'
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials'
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
+import Heading from '@ckeditor/ckeditor5-heading/src/heading'
+import Link from '@ckeditor/ckeditor5-link/src/link'
+import List from '@ckeditor/ckeditor5-list/src/list'
+import Table from '@ckeditor/ckeditor5-table/src/table'
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar'
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment'
+import FileUploadInline from './store/modules/plugins/inlineFileUpload'
+
+export default [
+    Essentials,
+    Paragraph,
+    Bold,
+    Italic,
+    Alignment,
+    Autoformat,
+    BlockQuote,
+    Heading,
+    Link,
+    List,
+    Table,
+    TableToolbar,
+    FileUploadInline
+]
--- src/store/modules/_editors.js
+++ src/store/modules/_editors.js
@@ -7,27 +7,8 @@
 import CodeFlask from 'codeflask'
 import loadScripts from './_loadScripts'
 import InlineEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor'
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat'
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials'
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote'
-import Heading from '@ckeditor/ckeditor5-heading/src/heading'
-import Link from '@ckeditor/ckeditor5-link/src/link'
-import List from '@ckeditor/ckeditor5-list/src/list'
-import Table from '@ckeditor/ckeditor5-table/src/table'
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar'
-import FileUploadInline from './plugins/inlineFileUpload'
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
-import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment'
-// eslint-disable-next-line import/no-cycle
+import CKPlugins from '../../ckplugins'
 import store from '../index'
-
-/**
- * Optionally store additional custom configuration in window
- * to customize the richtext editor for a specific society.
- */
-const customConfig = window.customConfig || {}
 
 const editors = {
   /**
@@ -70,6 +51,7 @@ const editors = {
 
       const appliedAttributes = {}
       const downloadNodes = [...node.querySelectorAll('a[download]')]
+      const ckconfig = window.ckconfig || {}
 
       if (downloadNodes.length) {
         downloadNodes.forEach(node => {
@@ -79,44 +61,8 @@ const editors = {
       }
 
       InlineEditor.create(node, {
-        plugins: [
-          Essentials,
-          Paragraph,
-          Bold,
-          Italic,
-          Alignment,
-          Autoformat,
-          BlockQuote,
-          Heading,
-          Link,
-          List,
-          Table,
-          TableToolbar,
-          FileUploadInline
-        ],
-        toolbar: {
-          items: [
-            'heading',
-            '|',
-            'alignment',
-            'bold',
-            'italic',
-            'link',
-            'bulletedList',
-            'numberedList',
-            'blockQuote',
-            'undo',
-            'redo',
-            'insertTable',
-            'fileUploadInline'
-          ]
-        },
-
-        // Society specific config
-        ...customConfig,
-        table: {
-          contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
-        }
+        plugins: CKPlugins,
+        ...ckconfig
       })
         .then(editor => {
           store.state.CKEditors.push(editor)
